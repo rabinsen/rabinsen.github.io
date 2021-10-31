1b0bcffd22e878eca9d141127ed063eb8c693ef7
rabinsen <rivansen0071@gmail.com>
2021-10-31 13:32:51 -0500

    Lab06 added without validation

---
+++ lab06/logic.js
@@ -0,0 +1,26 @@
+
+/* Question 3 */
+window.onload = function(){
+    var element1 = document.getElementById("alert1");
+    element1.onclick = function(){
+        setTimeout(booyah1, 2000);
+        document.getElementById("showAlert1").innerHTML = "Wait for Alert";
+    }
+
+    function booyah1(){
+        alert("BOOYAH!");
+    }
+
+    var element2 = document.getElementById("alert2");
+    element2.onclick = function(){
+        setTimeout(booyah2(), 2000);
+        document.getElementById("showAlert2").innerHTML = "Wait for Alert";
+    }
+
+    function booyah2(){
+        alert("BOOYAH!");
+    }
+}
+
+
+  
---
+++ lab06/problems2/part1/decoratemytext.js
@@ -0,0 +1,90 @@
+"use strict";
+
+/* window.onload = function(){
+    var element = document.getElementById("decorate");
+    element.onclick = function(){
+        var text = document.getElementById("textbox").value;
+        document.getElementById("output").innerHTML = text;
+        var temp = document.getElementById("output");
+        temp.style.color = "red";
+        temp.style.fontSize = 40 + "px"; 
+
+    }
+} */
+/* alert("Hello World"); */
+window.onload = function () {
+    var element = document.getElementById("bigDecoration");
+    
+    element.onclick = function(){
+        setInterval(increaseTextSize, 500);
+    }
+
+
+    /* alert("Hello World"); */
+    /* var text = document.getElementById("textbox"); */
+    /* text.style.fontSize = 24 + "pt"; */
+    /*  var textsize = (text.style.fontSize);
+     console.log(textsize); */
+    /* textsize += 2;
+   
+    text.style.fontSize = textsize + "pt";*/
+
+    var check = document.getElementById("checkbling");
+    check.onchange = function () {
+        var text = document.getElementById("text");
+        if (check.checked) {
+            text.style.fontWeight = "bold";
+            text.style.color = "green";
+            text.style.textDecoration = "underline";
+            var background = document.getElementById("background");
+            background.style.backgroundImage = "url('back.jpeg')"
+            background.style.backgroundRepeat = "repeat";
+        } else {
+            text.style.fontWeight = "normal";
+            text.style.color = "black";
+            text.style.textDecoration = "none";
+        }
+    }
+
+    var button2 = document.getElementById("igpay");
+    button2.onclick = function(){
+        var text = document.getElementById("text").value;
+        for(var i=0; i<text.length;){
+            if(! isVowel(text[i].toLowerCase())){
+                text += text[i];
+                
+                text = text.substring(1);
+               
+            }else{
+                text += "ay";
+                break;
+            }
+        }
+        document.getElementById("text").value = text;
+    }
+
+    var button3 = document.getElementById("malkovitch");
+    button3.onclick = function(){
+        var text = document.getElementById("text").value;
+
+        if(text.length >= 5){
+            text = "Malkovich";
+        }
+
+        document.getElementById("text").value = text;
+    }
+
+}
+
+function increaseTextSize() {
+    var text = document.getElementById("text");
+    const size = parseInt(getComputedStyle(text).fontSize) * 0.75; /* convert pixels to points*/
+    text.style.fontSize = size + 2 + "pt";
+}
+
+function isVowel(x){
+    return  x == 'a' || x=='e' || x == 'i' || x=='o' || x == 'u';
+}
+
+
+
