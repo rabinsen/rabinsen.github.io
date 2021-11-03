81e4dd7490dfcb0690a53de2e95e68d47af951fe
rabinsen <rivansen0071@gmail.com>
2021-11-02 21:14:47 -0500

    rugby update

--- lab07/closure lab/Problem1.js
+++ lab07/closure lab/Problem1.js
@@ -1,19 +1,20 @@
 window.onload = function () {
     var clickEvent = document.getElementById("click");
-    clickEvent.onclick = function () {
+    clickEvent.onclick = displayMsg2;
+}
+timer = null;
+function displayMsg2() {
+    if (timer === null) {
+        timer = setInterval(rudyTimer, 1000);
+    } else {
+        clearInterval(timer);
         timer = null;
-        if (timer === null) {
-            timer = setInterval(rudyTimer, 1000);
-        } else {
-            clearInterval(timer); 
-            timer = null;
-        }
     }
-    function rudyTimer() {
-        var rudyTimer = (function (str) {
-            document.getElementById("output").innerHTML += " Rudy!";
-        }(" Rudy!"))
-    }
+}
+function rudyTimer() {
+    var rudyTimer = (function (str) {
+        document.getElementById("output").innerHTML += " Rudy!";
+    }(" Rudy!"))
 }
 
 
